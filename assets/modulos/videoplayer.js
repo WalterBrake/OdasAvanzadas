fda4239b5515782f4fc1d2127118008e29e19657
Albin Rdz <ealbinu@gmail.com>
2020-08-01 09:35:03 -0500

    Arreglado

---
+++ assets/modulos/videoplayer.js
@@ -0,0 +1,29 @@
+Vue.component('videoplayer', {
+    props: [
+        'initclass', // clase inicial
+        'autoplay', // autoplay
+        'c', // color
+        'path' // Ruta hacia el archivo (sin extensión, debe haber un mp4 y un webm)
+
+    ],
+    data () {
+        return {
+
+        }
+    },
+    template: `
+        <div :class="'videoplayer ' + (initclass!=undefined?initclass:'') + ' ' + (c!=undefined?'c'+c:'')" ref="video">
+            <video :autoplay="autoplay" playsinline controls class="w100">
+                <source :src="path+'.mp4'" type="video/mp4" />
+                <source :src="path+'.webm'" type="video/webm" />
+            </video>
+        </div>
+    `,
+    mounted() {
+        const player = new Plyr('video', {
+            controls: ['play','progress', 'fullscreen'],
+            //iconUrl: '../../assets/aanim/plyr.svg'
+        })
+    }
+})
+
