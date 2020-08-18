c5c3d37f7cf7e3bc9e31d74ec3aef9b05c164c1f
Albin Rdz <ealbinu@gmail.com>
2020-08-18 09:18:24 -0500

    Text Module

--- assets/modulos/DrawEditor.js
+++ assets/modulos/DrawEditor.js
@@ -8,6 +8,8 @@ Vue.component('DrawEditor', {
             ctx: null,
             canvas: null,
             ctx: null,
+            tools: ['brush', 'text'],
+            currentTool: 'brush',
             colors: ['#5EB246', '#005093', '#EB8B2E', '#DB3E34', '#444', '#fff'],
             currentcolor: '#5EB246',
             sizes: [10, 6, 2],
@@ -15,25 +17,47 @@ Vue.component('DrawEditor', {
             scope: null,
             realSize: {width:0, height: 0},
             currentSize: {width:0, height: 0},
+            inputTextOn: false,
+            inputText: 'Texto',
+            inputTextPoint: {x:0, y:0},
+            inputTextPointScreen: {x:0, y:0},
+
         }
     },
     template: `
         <div class="draw-editor">
             <canvas class="drawEditorCanvas " ref="canvas" id="drawEditorCanvas" @mousedown="mouseDown" @touchstart="mouseDown"></canvas>
+            
+            <div class="inputText" v-if="inputTextOn && currentTool=='text' && currentcolor!='#fff'" :style="'border-color:'+currentcolor+'; left:'+inputTextPointScreen.x+'px; top:'+inputTextPointScreen.y+'px; margin-top:-'+returnTextSize+'px;'">
+                <div class="row">
+                    <input ref="inputTextInput" v-model="inputText" :style="'color:'+currentcolor+';' + ' font-size:'+returnTextSize+'px;' " />
+                    <button :disabled="inputText.length<1" class="button" :style="'background-color:'+currentcolor+';'" @click="createText"><img src="aimg/ok.svg"></button>
+                </div>
+            </div>
+            
             <div class="botonera" :style="'background-color:'+currentcolor+';'">
+                <div class="tools">
+                    <div v-for="to in tools" :style="'background-color:'+currentcolor+';'" :class="'tool tool_'+to + ' ' + (to==currentTool ? 'on':'off')" @click="setTool($event, to)"></div>
+                </div>
                 <div class="colors">
                     <div v-for="col in colors" class="color" :style="'background-color:'+col+';'" @click="setColor($event, col)"></div>
                 </div>
-                <div class="sizes">
+                <div class="sizes row wrap">
                     <div v-for="sz in sizes" :style="'background-color:'+currentcolor+';'" :class="'size size_'+sz + ' ' + (sz==currentBrushSize ? 'on':'off')" @click="setSize($event, sz)"></div>
                 </div>
-                <button class="button" @click="cleanCanvas(this)">Limpiar</button>
-                <button class="button" @click="downloadCanvas(this)">Descargar</button>
+                <div class="buttons row wrap">
+                    <button class="button" @click="cleanCanvas(this)">Limpiar</button>
+                    <button class="button" @click="downloadCanvas(this)">Descargar</button>
+                </div>
             </div>
         </div>
     `,
     computed: {
-        
+        returnTextSize(){
+            if(this.currentBrushSize==10){ return 28 }
+            if(this.currentBrushSize==6){ return 20 }
+            if(this.currentBrushSize==2){ return 15 }
+        },
     },
     methods: {
         setColor(e, col){
@@ -44,6 +68,11 @@ Vue.component('DrawEditor', {
         setSize(e, sz){
             s_select.play()
             this.currentBrushSize = sz
+            app.particleAnimation(e, null, null, 60)
+        },
+        setTool(e, to){
+            s_select.play()
+            this.currentTool = to
             app.particleAnimation(e, null, null, 60)
         },
         initCanvas () {
@@ -116,6 +145,29 @@ Vue.component('DrawEditor', {
             var realPos= { x: rw, y: rh }
             return realPos
         },
+        getRealPos (event) {
+            const drawEObj = document.getElementsByClassName('draw-editor')[0]
+            this.currentSize = {width: drawEObj.clientWidth, height: drawEObj.clientHeight, }
+            let rw = ((this.realSize.width)/100) * ((event.event.layerX*100)/this.currentSize.width)
+            let rh = ((this.realSize.height)/100) * ((event.event.layerY*100)/this.currentSize.height)
+            var realPos= { x: rw, y: rh }
+            return realPos
+        },
+        createInputText(point, ev){
+            this.inputTextOn = true
+            this.inputTextPoint = point
+            this.inputTextPointScreen = {x: ev.event.layerX - window.scrollX, y: ev.event.layerY - window.scrollY}
+            var _this = this
+            setTimeout(function(){_this.$refs.inputTextInput.focus()},100)
+        },
+        createText() {
+            var txt = new paper.PointText(this.inputTextPoint)
+            txt.fillColor = this.currentcolor
+            txt.fontSize = this.returnTextSize
+            txt.content = this.inputText
+            this.inputTextOn = false
+            this.inputText = 'Texto'
+        },
         mouseDown() {
             // in order to access functions in nested tool
             let self = this
@@ -123,27 +175,27 @@ Vue.component('DrawEditor', {
             // create drawing tool
             this.tool = this.createTool(this.scope)
             this.tool.onMouseDown = (event) => {
-                var realPos = self.getRealPos(event)
+                    var realPos = self.getRealPos(event)
+                    var hitResult = this.scope.project.hitTest(realPos, {
+                        segments: true,
+                        stroke: true,
+                        fill: true,
+                        tolerance: 5
+                    })
+                    if(hitResult && self.currentcolor=='#fff'){ if(hitResult.type=="stroke" || hitResult.type == "fill"){ hitResult.item.remove() } }
 
-                var hitResult = this.scope.project.hitTest(realPos, {
-                    segments: true,
-                    stroke: true,
-                    fill: true,
-                    tolerance: 5
-                })
-                if(hitResult && self.currentcolor=='#fff'){ if(hitResult.type=="stroke"){ hitResult.item.remove() } }
-
-                if(self.currentcolor!='#fff'){
-                    // init path
-                    self.path = self.pathCreate(self.scope)
-                    // add point to path
-                    self.path.add(realPos)
-                }
+                    if(self.currentcolor!='#fff' && self.currentTool=='brush'){
+                        // init path
+                        self.path = self.pathCreate(self.scope)
+                        // add point to path
+                        self.path.add(realPos)
+                    } else if(self.currentcolor!='#fff' && self.currentTool == 'text'){
+                        self.createInputText(realPos, event)
+                    }
             }
             this.tool.onMouseDrag = (event) => {
                 var realPos = self.getRealPos(event)
                 
-                
                 var hitResult = this.scope.project.hitTest(realPos, {
                     segments: true,
                     stroke: true,
@@ -151,7 +203,7 @@ Vue.component('DrawEditor', {
                     tolerance: 5
                 })
                 if(hitResult && self.currentcolor=='#fff'){ if(hitResult.type=="stroke"){ hitResult.item.remove() } }
-                if(self.currentcolor!='#fff'){
+                if(self.currentcolor!='#fff' && self.currentTool=='brush' ){
                     self.path.add(realPos)
                 }
                 //app.particleAnimation(event.event, 1, 2000, 10, self.currentcolor)
@@ -161,7 +213,7 @@ Vue.component('DrawEditor', {
                 var realPos = self.getRealPos(event)
 
                 // line completed
-                if(self.currentcolor!='#fff'){
+                if(self.currentcolor!='#fff' && self.currentTool=='brush'){
                     self.path.simplify(10)
                     self.path.add(realPos)
                 }
