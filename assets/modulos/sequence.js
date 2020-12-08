22d864cd8862d7fc064782c13343fa3b3582e00c
Albin Rdz <ealbinu@gmail.com>
2020-12-08 16:25:29 -0600

    MA6T2-141

--- assets/modulos/sequence.js
+++ assets/modulos/sequence.js
@@ -8,6 +8,7 @@ Vue.component('sequence', {
     methods: {
         adelante (){
             if(this.current==this.options.length-1){
+                console.log('last')
                 return false
             }
             this.current++
@@ -23,12 +24,15 @@ Vue.component('sequence', {
             if(this.current == this.options.length-1){
                 this.$emit('completed')
             }
+            if(this.current == this.options.length-1){
+                this.$emit('last')
+            }
         }
     },
     template: `
         <div :class="'sequence ' + (initclass?initclass:' ')">
             
-            <div v-for="(option, index) in options" v-if="index == current">
+            <div v-for="(option, index) in options" v-show="index == current">
                 <slot name="option" :option="option" />
             </div>
 
@@ -37,6 +41,9 @@ Vue.component('sequence', {
                 <button class="button" v-if="current==0" disabled>Atras</button>
                 <!--navigations-->
                 <button class="button big" v-if="current>0" @click="atras">Atras</button>
+
+
+                <!-- #### adelante --->
                 <button class="button big animate__animated animate__pulse animate__infinite" v-if="current==0" @click="adelante">Adelante</button>
                 <button class="button big" v-else-if="current<(options.length-1)" @click="adelante">Adelante</button>
                 <!--disabled-->
