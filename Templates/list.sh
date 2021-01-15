6432852487e2767ef0da1e030521e4678182476b
Albin Rdz <ealbinu@gmail.com>
2021-01-15 13:39:47 -0600

    fix

--- Templates/list.sh
+++ Templates/list.sh
@@ -1,4 +1,10 @@
-for value in MA1T3-165 MA1T3-169 ES2T3-95 MA2T3-213 MA2T3-216 CM2T3-291 ES3T3-80
+for folders in MA1T3 MA2T3 CM2T3 ES2T3 ES3T3 
+do
+    mkdir $folders
+done
+
+
+for value in MA1T3/165 MA1T3/169 MA2T3/213 CM2T3/291 ES2T3/95 ES3T3/80 MA2T3/216
 do
     echo $value
     cp -R tempPug/ $value
